7a55262b55c94a88ef5e47c26df8de37ebfb1931
Saz <sazi.nyathi@lexisnexis.co.za>
2020-07-25 14:21:41 +0200

    DEV: Created MCV Web Application

---
+++ TritonExpress/TritonExpress.Models/Department.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TritonExpress.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Required]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string DepartmentName { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ TritonExpress/TritonExpress.Models/Provinces.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TritonExpress.Models
+{
+    [Table("Provinces")]
+    public class Province
+    {
+        [Required]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+
+    }
+}
